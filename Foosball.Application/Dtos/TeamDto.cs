ed952b749c59c36a7f8de1e964b6d827cf783b4a
Mech0z <2316684+Mech0z@users.noreply.github.com.>
2025-08-09 21:45:49 +0200

    fix dto mapping issues due to missing paramless constructor

--- Foosball.Application/Dtos/TeamDto.cs
+++ Foosball.Application/Dtos/TeamDto.cs
@@ -1,4 +1,9 @@
 ﻿namespace Foosball.Application.Dtos
 {
-    public record TeamDto(string Defender, string Attacker);
+    public record TeamDto(string Defender, string Attacker)
+    {
+        public TeamDto() : this(string.Empty, string.Empty) { }
+        public TeamDto(string playerName)
+            : this(playerName, playerName) { }
+    }
 }
